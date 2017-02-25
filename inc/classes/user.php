79d251c93a06e227553aa01be2b03ed471c0774d
Nick Farley <nick@nfreader.net>
2017-02-25 16:58:19 -0500

    Player view
    Woo

--- inc/classes/user.php
+++ inc/classes/user.php
@@ -15,6 +15,7 @@ class user {
       $this->valid = FALSE;
       return $user;
     }
+    $user->bans = FALSE;
     $user = $this->parseUser($user);
     foreach ($user as $key=>$value){
       $this->$key = $value;
@@ -42,10 +43,13 @@ class user {
 
   public function parseUser(&$user){
     $user->legit = FALSE;
+    $user->color = "#FFF";
+    $user->colorFore = "#000";
+    $user->standing = 'Not banned';
     switch($user->lastadminrank) {
       default:
       case 'Player':
-        $user->label = "";
+        $user->label = "$user->ckey";
         $user->legit = FALSE;
       break;
 
@@ -65,6 +69,7 @@ class user {
       case 'GameMaster':
       case 'ClockcultEmpress':
         $user->color = '#9570c0';
+        $user->colorFore = '#FFF';
         $user->label = "<span class='label' style='background: $user->color'";
         $user->label.= "title='$user->lastadminrank'>";
         $user->label.= "<i class='fa fa-eye'></i> $user->ckey </span>";
@@ -113,6 +118,17 @@ class user {
       $user->legit = FALSE;
       $user->label = FALSE;
     }
+    $user->firstSeenTimeStamp = timeStamp($user->firstseen);
+    $user->lastSeenTimeStamp = timeStamp($user->lastseen);
+    if (!$user->bans){
+      $user->standing = 'Not banned';
+    } else {
+      foreach ($user->bans as $b) {
+        if ($b->status == 'Active'){
+          $user->standing = 'Banned';
+        }
+      }
+    }
     return $user;
   }
 
@@ -136,4 +152,49 @@ class user {
     return $db->single();
   }
 
+  public function getPlayerByCkey($ckey){
+    $db = new database();
+    if($db->abort){
+      return FALSE;
+    }
+    $db->query("SELECT ss13player.*,
+      TIMESTAMPDIFF(HOUR, lastseen, NOW()) AS hoursAgo,
+      count(DISTINCT ss13connection_log.id) AS connections,
+      count(DISTINCT ss13connection_log.ip) AS IPs,
+      count(DISTINCT ss13connection_log.computerid) AS CIDs
+      FROM ss13player
+      LEFT JOIN ss13connection_log ON ss13connection_log.ckey = ss13player.ckey
+      WHERE ss13player.ckey = ?");
+    $db->bind(1,strtolower(preg_replace('~[^a-zA-Z0-9]+~', '', $ckey)));
+    try {
+      $db->execute();
+    } catch (Exception $e) {
+      return returnError("Database error: ".$e->getMessage());
+    }
+    $player = $db->single();
+    $player->bans = $this->getPlayerBans($player->ckey);
+    $player = $this->parseUser($player);
+    return $player;
+  }
+
+  public function getPlayerBans($ckey){
+    $db = new database();
+    if($db->abort){
+      return FALSE;
+    }
+    $db->query("SELECT *
+      FROM ss13ban WHERE ckey=? ORDER BY bantime DESC");
+    $db->bind(1,strtolower(preg_replace('~[^a-zA-Z0-9]+~', '', $ckey)));
+    try {
+      $db->execute();
+    } catch (Exception $e) {
+      return returnError("Database error: ".$e->getMessage());
+    }
+    $ban = new ban();
+    if(!$bans = $db->resultSet()) return false;
+    foreach ($bans as &$b){
+      $b = $ban->parseBan($b);
+    }
+    return $bans;
+  }
 }
---
+++ tgdb/viewPlayer.php
@@ -0,0 +1,83 @@
+<?php require_once("../header.php");?>
+
+<?php require_once('tgdb_nav.php');?>
+
+<?php
+$ckey = false;
+if (isset($_GET['ckey'])){
+  $ckey = filter_input(INPUT_GET, 'ckey',
+    FILTER_SANITIZE_STRING,FILTER_FLAG_STRIP_HIGH);
+}
+
+if (!$ckey){
+  die(alert("Ckey not found.",FALSE));
+}
+
+$player = new user();
+$player = $player->getPlayerByCkey($ckey);
+?>
+
+<div class="page-header">
+  <h1><?php echo $player->label;?>&nbsp;
+  <small>Last seen <?php echo $player->hoursAgo;?> hours ago | 
+  <a href="https://www.byond.com/members/<?php echo $player->ckey;?>"
+  target="_blank">BYOND <i class="fa fa-external-link"></i></a>
+  </small></h1>
+</div>
+
+<div class="row">
+  <div class="col-md-4">
+    <ul class="list-group">
+      <li class="list-group-item" style="background: <?php echo $player->color;?>; color: <?php echo $player->colorFore;?>;">
+        <strong class="list-group-item-heading">Last rank</strong>
+          <?php echo $player->lastadminrank;?>
+      </li>
+      <?php if('Not banned' != $player->standing):?>
+        <li class="list-group-item list-group-item-danger">
+      <?php else:?>
+        <li class="list-group-item list-group-item-success">
+      <?php endif;?>
+        <strong class="list-group-item-heading">Account standing</strong>
+          <?php echo $player->standing;?>
+      </li>
+      <li class="list-group-item">
+        <strong class="list-group-item-heading">First seen</strong>
+          <?php echo $player->firstSeenTimeStamp;?>
+      </li>
+      <li class="list-group-item">
+        <strong class="list-group-item-heading">Last seen</strong>
+          <?php echo $player->lastSeenTimeStamp;?>
+      </li>
+      <li class="list-group-item">
+        <strong class="list-group-item-heading">Total Connections</strong>
+          <?php echo $player->connections;?>
+      </li>
+    </ul>
+  </div>
+  <div class="col-md-4">
+    <ul class="list-group">
+<!--       <li class="list-group-item list-group-item-success">
+        <strong class="list-group-item-heading">(<i class="fa fa-flask"></i>) Grief Index™</strong>
+          0
+      </li> -->
+      <li class="list-group-item">
+        <strong class="list-group-item-heading">Last IP</strong>
+          <?php echo $player->ip;?> <div class="ql">(<a href="#"><i class="fa fa-ban"></i></a>)(<a href="#"><i class="fa fa-plug"></i></a>)(<a href="#"><i class="fa fa-user"></i></a>)(<a href="#"><i class="fa fa-search"></i></a>)</div>
+      </li>
+      <li class="list-group-item">
+        <strong class="list-group-item-heading">Last CID</strong>
+          <?php echo $player->computerid;?> <div class="ql">(<a href="#"><i class="fa fa-ban"></i></a>)(<a href="#"><i class="fa fa-plug"></i></a>)(<a href="#"><i class="fa fa-user"></i></a>)</div>
+      </li>
+      <li class="list-group-item">
+        <strong class="list-group-item-heading">IPs seen</strong>
+          <?php echo $player->IPs;?>
+      </li>
+      <li class="list-group-item">
+        <strong class="list-group-item-heading">CIDs seen</strong>
+          <?php echo $player->CIDs;?>
+      </li>
+    </ul>
+  </div>
+</div>
+
+<?php require_once('../footer.php');?>
