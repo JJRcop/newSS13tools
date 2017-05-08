0ef7fd2c7d4c5e7ae6882136f7dfbe83e49d5429
Nick Farley <nick@nfreader.net>
2017-05-08 17:19:41 -0400

    And now a word from poly
    But only if you're logged in because Poly is VERY RUDE

--- index.php
+++ index.php
@@ -14,6 +14,10 @@ if(1 <= $interval) {
 <div class="page-header">
   <h1>SS13 Tools &amp; Stats</h1>
 </div>
+<?php if ($user->legit):?>
+  <?php $line = pick(json_decode(file_get_contents(ROOTPATH."/tmp/poly.json"),TRUE)['data']);?>
+  <div id="poly" class="engradio">[Poly] &ldquo;<?php echo $line;?>&rdquo; <img src="icons/animal/parrot_sit-1.png" height="64" width="64"  alt="And now a word from Poly" /></div>
+<?php endif;?>
 <?php $num = $app->getBigNumbers();?>
 <div class="row">
   <div class="col-md-6">
